cd4bfecbe9f26947358cae24c2686d3c4bd6bb5c
joaquin <jm622@duke.edu>
2020-07-16 19:42:20 -0300

    SQL scripts para construir una base de datos usando SQLite3

--- SQL/create_tables_provincia_localidad.sql
+++ SQL/create_tables_provincia_localidad.sql
@@ -11,3 +11,13 @@ CREATE TABLE IF NOT EXISTS localidad (
   FOREIGN KEY (l_id) REFERENCES provincia(p_id)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS comunas_capital (
+    c_id TINYINT NOT NULL PRIMARY KEY,
+    provincia_id TINYINT  NOT NULL ,
+    barrio ARCHAR(50) NOT NULL,
+    comuna INT(2) NOT NULL,
+    codigopostal SMALLINT(6) NOT NULL,
+    FOREIGN KEY (c_id) REFERENCES provincia(p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
