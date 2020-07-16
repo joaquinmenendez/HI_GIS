c1eafa98e9634774faa64aeb0b445f89f6db99cf
joaquin <jm622@duke.edu>
2020-07-16 19:45:48 -0300

    post request fucntion created

--- get_centroides.py
+++ get_centroides.py
@@ -1,6 +1,8 @@
 import requests
 import urllib
 
+API_BASE_URL = "https://apis.datos.gob.ar/georef/api/"
+
 def get_centroides(endpoint, nombre, **kwargs):
     '''
     La funcion toma un endpoint y un nombre y realiza una request a la API del gobierno argentino
@@ -43,3 +45,34 @@ def get_localidad(provincia,localidad):
     '''
     list_localidades = get_centroides('localidades', localidad)
     return filter_loc(list_localidades, provincia)
+
+
+def post_centroides(endpoint, prov_loc, prov = True):
+    """
+    Realiza un POST para una lista de provincias y localidades. La funcion retorna una lista con la localidad mas probable para esa combinacion.
+    En caso de que no se desee proveer las provincias la API inferira la localidad solo con el nombre.
+    :param endpoint: str
+    :param prov_loc: list[(str,str)] o list[str]
+    'param prov: boolean default True
+    :returns: list[dict]
+    """
+    API_BASE_URL = "https://apis.datos.gob.ar/georef/api/"
+    # realiza consulta a la API
+    if prov:
+        data = {
+                endpoint: [ {"nombre": loc, "max": 1, 'provincia' : prov} for prov,loc in prov_loc]
+                }
+    else:
+        data = {
+                endpoint: [ {"nombre": loc, "max": 1} for loc in prov_loc]
+                }
+    url = API_BASE_URL + endpoint
+    results = requests.post(url, json = data, headers={"Content-Type": "application/json"}).json()
+
+    # convierte a una lista de "resultado más probable" o "vacío" cuando no hay
+    parsed_results = [
+        single_result[endpoint][0] if single_result[endpoint] else {}
+        for single_result in results["resultados"]
+    ]
+
+    return parsed_results
